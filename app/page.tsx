c59730272e6883ee3a8b8b865e5f9d2fc7cf7ee3
kanybek-kanybek <kanybekzheentaev@gmail.com>
2024-08-29 17:18:17 +0600

    start home

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,14 @@
 import Footer from '@/components/header/footer/footer';
 import Header from '@/components/header/header';
+import MainBlock from '@/components/MainBLokc/MainBlock';
+import ProductBlock from '@/components/ProductBlock/ProductBlock';
 
 const Home: React.FC = () => {
   return (
     <>
       <Header />
+      <MainBlock />
+      <ProductBlock />
       <Footer />
     </>
   );
---
+++ components/ProductBlock/ProductBlock.tsx
@@ -0,0 +1,44 @@
+import Image from 'next/image';
+import img1 from '../../public/assets/img1.svg';
+import img2 from '../../public/assets/img2.svg';
+import img3 from '../../public/assets/img3.svg';
+import './ProductBlock.css';
+export default function ProductBlock() {
+  return (
+    <div>
+      <section id="product">
+        <div className="container">
+          <div className="product">
+            <Image src={img1} alt="" />
+            <div className="productTime">
+              <div className="productTImeH">
+                <h1>Flash Sales</h1>
+                <div>
+                  <h6>Days</h6>
+                  <h4>03</h4>
+                </div>
+                <div>
+                  <h6>Hours</h6>
+                  <h4>23</h4>
+                </div>
+                <div>
+                  <h6>Minutes</h6>
+                  <h4>19</h4>
+                </div>
+                <div>
+                  <h6>Seconds</h6>
+                  <h4>56</h4>
+                </div>
+              </div>
+              <div className="productTimeImage">
+                <Image src={img2} alt="" />
+                <Image src={img3} alt="" />
+              </div>
+            </div>
+            <div className="productImage"></div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
